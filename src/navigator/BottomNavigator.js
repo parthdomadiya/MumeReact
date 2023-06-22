af57f739b89b437073c671b424eac1ff0cd471fe
Shruti1230 <shrutikathiriya7@gmail.com>
2023-06-22 12:30:32 +0530

    bottom animation

--- src/navigator/BottomNavigator.js
+++ src/navigator/BottomNavigator.js
@@ -1,103 +1,133 @@
-import React,{useContext,useState,useRef,useEffect} from 'react';
-import {StyleSheet,Text,View,} from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from "react-native-vector-icons/Ionicons"
-import { Colors } from '../theme/color';
+import React, { useEffect, useRef } from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import Icon, { Icons } from '../screens/Icons';
 import themeContext from '../theme/themeContex';
-import Icon from 'react-native-vector-icons/FontAwesome5'
-import Icons from 'react-native-vector-icons/MaterialCommunityIcons'
+
+import { Colors } from '../theme/color';
+import * as Animatable from 'react-native-animatable';
+import style from '../theme/style';
 import Home from '../screens/Home';
 import Favorites from '../screens/Favorites';
 import Playlist from '../screens/Playlist';
 import Setting from '../screens/Setting';
-import Animated, { FadeInUp, FadeOutDown, Layout } from 'react-native-reanimated';
+
+const TabArr = [
+  { route: 'Home', label: 'Home', type: Icons.Ionicons, icon: 'home-outline', component: Home },
+  { route: 'Favorites', label: 'Favorites', type: Icons.Ionicons, icon: 'heart-outline', component: Favorites },
+  { route: 'Playlist', label: 'Playlist', type: Icons.Ionicons, icon: 'reader-outline', component: Playlist },
+  { route: 'Setting', label: 'Setting', type: Icons.Ionicons, icon: 'settings-outline', component: Setting },
+];
 
 const Tab = createBottomTabNavigator();
 
-export default function MyTabs() {
+const animate1 = { 0: { scale: .5, translateY: 7 }, .92: { translateY: -34 }, 1: { scale: 1.2, translateY: -20 } }
+const animate2 = { 0: { scale: 1.2, translateY: -20 }, 1: { scale: 1, translateY: 7 } }
 
-  const theme = useContext(themeContext);
-  const [darkMode,setDarkMode] = useState('false')
+const circle1 = { 0: { scale: 0 }, 0.3: { scale: .9 }, 0.5: { scale: .2 }, 0.8: { scale: .7 }, 1: { scale: 1 } }
+const circle2 = { 0: { scale: 1 }, 1: { scale: 0 } }
+
+const TabButton = (props) => {
+  const { item, onPress, accessibilityState } = props;
+  const focused = accessibilityState.selected;
+  const viewRef = useRef(null);
+  const circleRef = useRef(null);
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    if (focused) {
+      viewRef.current.animate(animate1);
+      circleRef.current.animate(circle1);
+      textRef.current.transitionTo({ scale: 1 });
+    } else {
+      viewRef.current.animate(animate2);
+      circleRef.current.animate(circle2);
+      textRef.current.transitionTo({ scale: 0 });
+    }
+  }, [focused])
 
   return (
-  //   <Animated.View
-  //     entering={FadeInUp}
-  //     exiting={FadeOutDown}
-  //     layout={Layout.duration(100)}
-  //     style={{
-  //       height:80,
-  //     }}
-  // >
-    
-    <Tab.Navigator
-    screenOptions={{
-      tabBarStyle: { position: 'absolute',height:70,paddingBottom:10,paddingTop:10,backgroundColor:theme.bottom,},
-      tabBarLabelStyle: {
-        fontSize: 15,
-      },
-      tabBarShowLabel:false,
-    }}
-    >
-   
-    <Tab.Screen name="Home" component={Home} 
-    options={{
-      tabBarShowLabel:true,
-      tabBarLabel: ({focused, color,}) => (
-        <Text style={{color: focused ? Colors.primary : Colors.disable,fontFamily:'Urbanist-Regular'}}>Home</Text>
-      ),
-      tabBarIcon:({focused,color}) => {
-        return <Ionicons name={focused ? 'home' : 'home-outline'} size={25} 
-        color={focused ? Colors.primary : Colors.disable} />
-      },
-      headerShown:false,
-      // animationTypeForReplace: 'push',
-      //  animation:'slide_from_bottom'
-    }}
-    />
-    
-  
-    <Tab.Screen name="Favorites" component={Favorites}
-     options={{
-      tabBarShowLabel:true,
-      tabBarLabel: ({focused, color,}) => (
-        <Text style={{color: focused ? Colors.primary : Colors.disable,fontFamily:'Urbanist-Regular'}}>Favorites</Text>
-      ),
-      tabBarIcon:({focused,color}) => {
-        return <Ionicons name={focused ? 'heart' : 'heart-outline'} size={25}
-        color={ focused ? Colors.primary : Colors.disable} />
-      },
-      headerShown:false,
-    }} />
-
- <Tab.Screen name="Playlist" component={Playlist}
-     options={{
-      tabBarShowLabel:true,
-      tabBarLabel: ({focused, color,}) => (
-        <Text style={{color: focused ? Colors.primary : Colors.disable,fontFamily:'Urbanist-Regular'}}>Playlists</Text>
-      ),
-      tabBarIcon:({focused,color}) => {
-        return <Ionicons name={focused ? 'reader' : 'reader-outline'} size={25}
-        color={focused ? Colors.primary : Colors.disable} />
-      },
-      headerShown:false,
-    }} />
-
-<Tab.Screen name="Setting" component={Setting}
-    options={{
-      tabBarShowLabel:true,
-      tabBarLabel: ({focused, color,}) => (
-        <Text style={{color: focused ? Colors.primary : Colors.disable,fontFamily:'Urbanist-Regular'}}>Settings</Text>
-      ),
-      tabBarIcon:({focused,color}) => {
-        return <Ionicons name={focused ? 'settings' : 'settings-outline'} size={25}
-        color={focused ? Colors.primary: Colors.disable} />
-      },
-      headerShown:false,
-    }} />
-
-  </Tab.Navigator>
-  // </Animated.View>
-  );
+    <TouchableOpacity
+      onPress={onPress}
+      activeOpacity={1}
+      style={styles.container}>
+      <Animatable.View
+        ref={viewRef}
+        duration={500}
+        style={styles.container}>
+        <View style={styles.btn}>
+          <Animatable.View
+            ref={circleRef}
+            style={styles.circle} />
+          <Icon type={item.type} name={item.icon} color={focused ? Colors.secondary : Colors.primary} />
+        </View>
+        <Animatable.Text
+          ref={textRef}
+          style={styles.text}>
+          {item.label}
+        </Animatable.Text>
+      </Animatable.View>
+    </TouchableOpacity>
+  )
 }
 
+export default function AnimTab1() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: styles.tabBar,
+      }}
+    >
+      {TabArr.map((item, index) => {
+        return (
+          <Tab.Screen key={index} name={item.route} component={item.component}
+            options={{
+              tabBarShowLabel: false,
+              tabBarButton: (props) => <TabButton {...props} item={item} />
+            }}
+          />
+        )
+      })}
+    </Tab.Navigator>
+  )
+}
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tabBar: {
+    height: 70,
+    position: 'absolute',
+    // bottom: 16,
+    // right: 16,
+    // left: 16,
+    // borderRadius: 16,
+  },
+  btn: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 4,
+    borderColor: Colors.secondary,
+    backgroundColor: Colors.secondary,
+    justifyContent: 'center',
+    alignItems: 'center',
+    
+  },
+  circle: {
+    ...StyleSheet.absoluteFillObject,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Colors.primary,
+    borderRadius: 25,
+  },
+  text: {
+    fontSize: 10,
+    textAlign: 'center',
+    color: Colors.primary,
+  }
+})
